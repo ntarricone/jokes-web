c62077843ebc4b6fa51e873e7e374cbd4d727aa0
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-08 13:36:49 +0100

    chore: add meta description in _documents

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="Fathom 3 Jokes web" />
+        <meta name="description" content="Mock Jokes web for Fathom 3 Task" />
+      </Head>
       <body>
         <Main />
         <NextScript />
