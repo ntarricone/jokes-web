0511525977c82a2aa7a2a8d4ac00b49997e18c94
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-08 13:36:12 +0100

    chore: add Bangers font to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Bangers } from '@next/font/google'
+import Head from 'next/head'
+
+const bangers = Bangers({
+  weight: '400',
+  display: 'auto',
+  preload: false,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={bangers.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
