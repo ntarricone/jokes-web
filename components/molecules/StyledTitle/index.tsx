95e04b6763b1285500a2eeaf0c2d0a7a69d0a49f
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-08 13:39:23 +0100

    feat: create StyledTitle

---
+++ components/molecules/StyledTitle/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { Text } from '../../atoms/text/text'
+
+export const StyledTitle = styled(Text)`
+  background: linear-gradient(
+    219deg,
+    #186cb8 19%,
+    #2a9a9f 20%,
+    #f1b211 59%,
+    #e83611 79%,
+    #f9002f 80%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
